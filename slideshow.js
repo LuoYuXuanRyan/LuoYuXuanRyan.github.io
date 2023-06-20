26c9478b8c3769069e2bc5ff3d83c13bc126a523
LuoYuXuanRyan <119206349+LuoYuXuanRyan@users.noreply.github.com>
2023-06-20 11:40:53 +0000

    feat: First commit Updated By Github Actions

--- slideshow.js
+++
@@ -1,9 +0,0 @@
-var index = 1;
-function show() {
-    index++;
-    if(index > 7) {
-        index = 1;
-    }
-    var img = document.getElementById("show");
-    img.src = "./pictures/" + index +".png";
-}   
